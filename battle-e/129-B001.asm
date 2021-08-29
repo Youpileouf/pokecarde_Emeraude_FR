e5df3e0a7afd40a65aa4247de717f3c513c3fe3f
Artrios <dapaquelet@hotmail.com>
2021-08-29 17:26:21 +0800

    Add the promo trainers

---
+++ battle-e/129-B001.asm
@@ -0,0 +1,4 @@
+INCLUDE "../macros.asm"
+CLASS EQUS   "gentleman"
+TRAINER EQUS "nils"
+INCLUDE "battletrainer-{REGION_NAME}.tx"
---
+++ battle-e/129-B002.asm
@@ -0,0 +1,4 @@
+INCLUDE "../macros.asm"
+CLASS EQUS   "lady"
+TRAINER EQUS "astrid"
+INCLUDE "battletrainer-{REGION_NAME}.tx"
