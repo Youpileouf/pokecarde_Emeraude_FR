72bd42607483cb8d1c071daa127e3dba14052441
Youpileouf <amaury.kasprzyk@gmail.com>
2025-08-17 16:23:48 +0200

    Adjustement to Emerald berry data structure

--- battle-e/berries/ginema.asm
+++ battle-e/berries/ginema.asm
@@ -6,8 +6,8 @@ INCLUDE "berries/macros.asm"
 	Firmness VERY_HARD
 	Size 3,5
 	Yield_Range 2, 3
-	db $2C,$A6,$58,$08 ; US ROM offset Enigma tag1
-	db $4A,$A6,$58,$08 ; US ROM offset Enigma tag2
+	BerryTag BERRY_TAG_ONE
+	BerryTag BERRY_TAG_TWO
 	Growth_Stage_Hours 18
 	Flavor 0, 30, 0, 0, 30
 	Smoothness 85
