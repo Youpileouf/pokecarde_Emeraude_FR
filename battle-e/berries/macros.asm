6eaf37bee20abcb1e1143104e769bed5934b92c0
Youpileouf <amaury.kasprzyk@gmail.com>
2025-08-17 16:17:36 +0200

    Adjustment for Emerald berry data structure

--- battle-e/berries/macros.asm
+++ battle-e/berries/macros.asm
@@ -4,7 +4,7 @@ MACRO Enigma_Berry
 	Section "berry",ROM0[$100]
 	db $01
 	dd $02000000
-	db REGION,0,REGION,0,0,0,$04,0,$80,$01,0,0
+	db $FF,0,$FF,0,0,0,$04,0,$80,$03,0,0
 	db $07
 	dd $02000018
 	db $02,$00
@@ -25,6 +25,10 @@ MACRO Yield_Range
 MACRO Size
 	dw \1 * 10 + \2
 	ENDM
+
+DEF BerryTag EQUS "db"
+DEF BERRY_TAG_ONE EQUS "$2C,$A6,$58,$08" ; EN ROM offset Enigma tag1
+DEF BERRY_TAG_TWO EQUS "$4A,$A6,$58,$08" ; EN ROM offset Enigma tag2
 
 DEF Growth_Stage_Hours EQUS "db"
 DEF Flavor EQUS "db"
